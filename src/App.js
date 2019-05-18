c4b22217697c8012352818dd6d8845d539cec37d
Marcus Menezes <mvgmenezes@gmail.com>
2019-05-18 20:16:43 -0300

    Criando o componente Search

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import React from 'react';
+import Search from './components/search';
 
 
 const App = () => (
   <div className="container-fluid">
 
     <div className=' container form-group'>
-      <div className="alert alert-primary" role="alert">
-        Digite o nome a ser pesquisado
-      </div>
-      <div className='search'>
-        <input type='form-control' placeholder='Pesquisar' />
-      </div>
 
-
+      <Search />
 
       <div className='user-info'>
-        <img src='https://avatars1.githubusercontent.com/u/29113078?v=4' />
+
+        <img src="https://avatars1.githubusercontent.com/u/29113078?v=4" className="figure-img img-fluid rounded" alt="..." />
 
         <h1 className='username'>
           <a href='https://github.com/mvgmenezes'>Marcus Menezes</a>
@@ -26,32 +22,33 @@ const App = () => (
           <li>Seguidores: 122</li>
           <li>Seguindo: 122</li>
         </ul>
-
-        <div className='actions'>
-          <button>Ver Repositorios</button>
-          <button>Ver favoritos</button>
-        </div>
-
-        <div className='repos'>
-          <h2>Repositorios:</h2>
-          <ul>
-            <li>
-              <a href='#'>Nome dos repositorios</a>
-            </li>
-          </ul>
-
-        </div>
-
-        <div className='starred'>
-          <h2>Favoritos:</h2>
-          <ul>
-            <li>
-              <a href='#'>Nome dos repositorios</a>
-            </li>
-          </ul>
-
-        </div>
       </div>
+
+      <div className='actions container form-group'>
+        <button className='btn btn-outline-primary'>Ver Repositorios</button>
+        <button className='btn btn-outline-primary'>Ver favoritos</button>
+      </div>
+
+      <div className='repos'>
+        <h2>Repositorios:</h2>
+        <ul>
+          <li>
+            <a href='#'>Nome dos repositorios</a>
+          </li>
+        </ul>
+
+      </div>
+
+      <div className='starred'>
+        <h2>Favoritos:</h2>
+        <ul>
+          <li>
+            <a href='#'>Nome dos repositorios</a>
+          </li>
+        </ul>
+
+      </div>
+
     </div>
   </div>
 
---
+++ src/components/search.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Search = () => (
+    <div className='search'>
+        <div className="alert alert-primary" role="alert">
+            Digite o nome a ser pesquisado
+        </div>
+        <input type='form-control' placeholder='Pesquisar' />
+    </div>
+)
+
+export default Search
