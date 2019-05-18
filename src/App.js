bdd9b45dffd66f50a27bc3eb7c72f02f20146144
Marcus Menezes <mvgmenezes@gmail.com>
2019-05-18 12:49:24 -0300

    Criando a pagina inicial

--- src/App.js
+++ src/App.js
@@ -1,12 +1,51 @@
 import React from 'react';
 
 
-function App() {
-  return (
-    <div className="App">
-      olamundo
+const App = () => (
+  <div className="app">
+
+    <div className='search'>
+      <input type='search' placeholder='Digite o nome do usuario' />
     </div>
-  );
-}
+
+    <div className='user-info'>
+      <img src='https://avatars1.githubusercontent.com/u/29113078?v=4' />
+      <h1>
+        <a href='https://github.com/mvgmenezes'>Marcus Menezes</a>
+      </h1>
+
+      <ul className='repos-info'> 
+        <li>Repositorios: 122</li>
+        <li>Seguidores: 122</li>
+        <li>Seguindo: 122</li>
+      </ul>
+
+      <div className='actions'> 
+        <button>Ver Repositorios</button>
+        <button>Ver favoritos</button>
+      </div>
+
+      <div className='repos'>
+        <h2>Repositorios:</h2>
+        <ul>
+          <li>
+            <a href='#'>Nome dos repositorios</a>
+          </li>
+        </ul>
+        
+      </div>
+
+      <div className='starred'>
+      <h2>Favoritos:</h2>
+      <ul>
+        <li>
+          <a href='#'>Nome dos repositorios</a>
+        </li>
+      </ul>
+        
+      </div>
+    </div>
+  </div>
+)
 
 export default App;
