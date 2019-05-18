8d9f0c313064ea05583d22f87f4598b1a03553f4
Marcus Menezes <mvgmenezes@gmail.com>
2019-05-18 13:06:08 -0300

    Atualizando o CSS

--- src/App.js
+++ src/App.js
@@ -2,50 +2,59 @@ import React from 'react';
 
 
 const App = () => (
-  <div className="app">
+  <div className="container-fluid">
 
-    <div className='search'>
-      <input type='search' placeholder='Digite o nome do usuario' />
-    </div>
-
-    <div className='user-info'>
-      <img src='https://avatars1.githubusercontent.com/u/29113078?v=4' />
-      <h1>
-        <a href='https://github.com/mvgmenezes'>Marcus Menezes</a>
-      </h1>
-
-      <ul className='repos-info'> 
-        <li>Repositorios: 122</li>
-        <li>Seguidores: 122</li>
-        <li>Seguindo: 122</li>
-      </ul>
-
-      <div className='actions'> 
-        <button>Ver Repositorios</button>
-        <button>Ver favoritos</button>
+    <div className=' container form-group'>
+      <div className="alert alert-primary" role="alert">
+        Digite o nome a ser pesquisado
+      </div>
+      <div className='search'>
+        <input type='form-control' placeholder='Pesquisar' />
       </div>
 
-      <div className='repos'>
-        <h2>Repositorios:</h2>
-        <ul>
-          <li>
-            <a href='#'>Nome dos repositorios</a>
-          </li>
+
+
+      <div className='user-info'>
+        <img src='https://avatars1.githubusercontent.com/u/29113078?v=4' />
+
+        <h1 className='username'>
+          <a href='https://github.com/mvgmenezes'>Marcus Menezes</a>
+        </h1>
+
+        <ul className='repos-info'>
+          <li>Repositorios: 122</li>
+          <li>Seguidores: 122</li>
+          <li>Seguindo: 122</li>
         </ul>
-        
-      </div>
 
-      <div className='starred'>
-      <h2>Favoritos:</h2>
-      <ul>
-        <li>
-          <a href='#'>Nome dos repositorios</a>
-        </li>
-      </ul>
-        
+        <div className='actions'>
+          <button>Ver Repositorios</button>
+          <button>Ver favoritos</button>
+        </div>
+
+        <div className='repos'>
+          <h2>Repositorios:</h2>
+          <ul>
+            <li>
+              <a href='#'>Nome dos repositorios</a>
+            </li>
+          </ul>
+
+        </div>
+
+        <div className='starred'>
+          <h2>Favoritos:</h2>
+          <ul>
+            <li>
+              <a href='#'>Nome dos repositorios</a>
+            </li>
+          </ul>
+
+        </div>
       </div>
     </div>
   </div>
+
 )
 
 export default App;
