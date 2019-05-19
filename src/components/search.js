b0f24bbba3955b3263c10d58e5f9cb7c31d85893
Marcus Menezes <mvgmenezes@gmail.com>
2019-05-18 22:26:11 -0300

    Implementando o request dos dados do usuario no search

--- src/components/search.js
+++ src/components/search.js
@@ -5,7 +5,21 @@ const Search = () => (
         <div className="alert alert-primary" role="alert">
             Digite o nome a ser pesquisado
         </div>
-        <input type='form-control' placeholder='Pesquisar' />
+        <input type='form-control' placeholder='Pesquisar' onKeyUp={(e) => {
+            
+            const value = e.target.value
+            //qual tecla foi pressinado
+            const key = e.which || e.keyCode
+            //igual a 13 o enter foi pressionado
+            const ENTER = 13
+
+            if (key==ENTER){
+                fetch(`https://api.github.com/users/${value}`).then(
+                    (result) => {
+                      console.log(result)
+                    })
+            }
+        }}/>
     </div>
 )
 
